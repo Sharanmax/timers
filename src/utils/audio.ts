70832e1958eafc22db660b8a7badef355c9cfa0f
Sharanmax <sharanraomax@gmail.com>
2024-12-21 21:02:25 +0530

    initial commit

--- src/utils/audio.ts
+++ src/utils/audio.ts
@@ -41,7 +41,7 @@ export class TimerAudio {
       // Configure gain (volume) envelope
       this.gainNode.gain.setValueAtTime(0, this.audioContext.currentTime);
       this.gainNode.gain.linearRampToValueAtTime(0.5, this.audioContext.currentTime + 0.01);
-      this.gainNode.gain.linearRampToValueAtTime(0, this.audioContext.currentTime + 0.5);
+     // this.gainNode.gain.linearRampToValueAtTime(0, this.audioContext.currentTime + 0.5);
       
       // Connect nodes
       this.oscillator.connect(this.gainNode);
@@ -49,12 +49,13 @@ export class TimerAudio {
       
       // Start and stop the oscillator
       this.oscillator.start(this.audioContext.currentTime);
-      this.oscillator.stop(this.audioContext.currentTime + 0.5);
+      //this.oscillator.stop(this.audioContext.currentTime + 0.5);
       
       // Cleanup after sound ends
-      setTimeout(() => {
-        this.cleanup();
-      }, 500);
+      // setTimeout(() => {
+      //   console.log('Inside setTimeout: ', this);
+      //   this.cleanup();
+      // }, 500);
 
     } catch (error) {
       console.error('Failed to play audio:', error);
@@ -66,6 +67,8 @@ export class TimerAudio {
   }
 
   private cleanup(): void {
+    console.log("check here context ---", this)
+
     if (this.oscillator) {
       try {
         this.oscillator.stop();
